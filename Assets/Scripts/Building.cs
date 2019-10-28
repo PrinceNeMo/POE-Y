227b0b4c717200cd1d3ceae9f9fdd8b23934c452
PrinceNeMo <56159288+PrinceNeMo@users.noreply.github.com>
2019-10-28 14:48:46 +0100

    hime

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Building : MonoBehaviour
+{
+    public enum heldReasources{ Stone }
+    public bool isSlected = false;
+    public string objectName;
+    private NavMeshAgent agent;
+    public int heldReasource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1) && isSlected)
+        {
+            RightClick();
+        }
+    }
+
+    void RightClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit,100))
+        {
+            if(hit.collider.tag == "Ground")
+            {
+                agent.destination = hit.point;
+                Debug.Log("moved");
+            }
+        }
+    }
+}
